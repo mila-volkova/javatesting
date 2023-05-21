9bf0f357adcc140dac9025ceafe3ddac2f6bf344
Neto test <lyudmila.volkova.70@mail.ru>
2023-05-21 22:39:15 +0800

    commit testng

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,37 +1,40 @@
-import static org.junit.Assert.*;
+
 
 public class CashbackHackServiceTest {
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void AddedAmountForCashback() {
         int amount = 500;
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
         int expected = 500;
-        org.junit.Assert.assertEquals(expected,actual);
+        org.testng.Assert.assertEquals(expected, actual);
     }
-    @org.junit.Test
+
+    @org.testng.annotations.Test
     public void AddedAmountForCashback1() {
         int amount = 999;
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
         int expected = 1;
-        org.junit.Assert.assertEquals(expected,actual);
+        org.testng.Assert.assertEquals(expected, actual);
     }
-    @org.junit.Test
+
+    @org.testng.annotations.Test
     public void AddedAmountForCashback2() {
         int amount = 1000;
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
         int expected = 0;
-        org.junit.Assert.assertEquals(expected,actual);
+        org.testng.Assert.assertEquals(expected, actual);
     }
-    @org.junit.Test
+
+    @org.testng.annotations.Test
     public void AddedAmountForCashback3() {
         int amount = 1001;
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
         int expected = 999;
-        org.junit.Assert.assertEquals(expected,actual);
+        org.testng.Assert.assertEquals(expected, actual);
     }
 
 }
